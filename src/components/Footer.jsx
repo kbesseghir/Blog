304e36506c31f5bcf8a9ffbd44461ff1a5211ae3
kbesseghir <k.besseghir@esi-sba.dz>
2024-01-06 11:34:47 +0100

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-gray-900 text-white'>
+    
+    <div className='px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-4'>
+    <div>
+    <div>
+    <div>
+    <p className='font-medium tracking-wide text-gray-300'> Category</p>
+    <ul>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>News</a></li></ul>
+    
+    </div></div>
+    </div>
+    </div></div>
   )
 }
 
