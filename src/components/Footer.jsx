0d1b43af3880c56b4ceb01641751034f63736c9f
kbesseghir <k.besseghir@esi-sba.dz>
2024-01-07 09:57:47 +0100

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,70 @@ const Footer = () => {
     
     <div className='px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-4'>
     <div>
-    <div>
+    <div className='grid grid-cols-2 gap-5 lg:col-span-4 md:grid-cols-4'>
     <div>
     <p className='font-medium tracking-wide text-gray-300'> Category</p>
-    <ul>
+    <ul className='mt-2 space-y-2'>
     <li>
-    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>News</a></li></ul>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>News</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>world</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Games</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Refernces</a></li>
+    </ul>
     
-    </div></div>
+    </div>
+    <div>
+    <p className='font-medium tracking-wide text-gray-300'> Apps</p>
+    <ul className='mt-2 space-y-2'>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Web</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Business</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>eCommerce</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Entertainment</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Portfolio</a></li>
+    </ul>
+    
+    </div>
+    <div>
+    <p className='font-medium tracking-wide text-gray-300'> Cherry</p>
+    <ul className='mt-2 space-y-2'>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Media</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Brochure</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>NonProfit</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Education</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Projects</a></li>
+    </ul>
+    
+    </div>
+    <div>
+    <p className='font-medium tracking-wide text-gray-300'> Business</p>
+    <ul className='mt-2 space-y-2'>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Infopreneur</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Personal</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Wiki</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Forum</a></li>
+    <li>
+    <a href='/' className='text-gray-500 transition-colors duration-300 hover:text-orange-500'>Portfolio</a></li>
+    </ul>
+    
+    </div>
+    </div>
     </div>
     </div></div>
   )
